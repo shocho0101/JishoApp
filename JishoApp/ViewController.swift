ae62d3b56b8de6d24c26a1381be4df02105ed8c1
choccho <shocho0101@hotmail.co.jp>
2020-02-01 01:34:58 +0900

    Initial Commit

---
+++ JishoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JishoApp
+//
+//  Created by 張翔 on 2020/02/01.
+//  Copyright © 2020 張翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
